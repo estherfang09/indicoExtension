0659f7487bc772eb004cd28c1950630b9cc2843d
esther.fang <estherfang09@gmail.com>
2016-05-17 20:42:57 -0400

    change to multiple api call

--- background.js
+++ background.js
@@ -3,16 +3,10 @@ chrome.contextMenus.onClicked.addListener(function(info,tab){
   var text = info.selectionText;
       console.log(text);
     $.post(
-      'https://apiv2.indico.io/sentiment/batch?key=f52f3bcce1f70fa713775b1f5846ed8e',
-      JSON.stringify({
-      'data': text
+    'https://apiv2.indico.io/apis/multiapi/batch?key=f52f3bcce1f70fa713775b1f5846ed8e&apis=sentimenthq,texttags,language,keywords',
+     JSON.stringify({'data': text
     })
     ).then(function(res) { console.log("w" + res) });
-    $.post(
-      'https://apiv2.indico.io/language?key=f52f3bcce1f70fa713775b1f5846ed8e',
-      JSON.stringify({
-      'data': text
-    })
-).then(function(res) { console.log(res) });
+   
 
 })
